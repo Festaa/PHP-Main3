f0ee3b5a390f5a0eff91955236d15bcde09a1e10
Festaa <Festa.Rexhepi@digitalschool.tech>
2025-12-22 17:41:30 +0100

    commit

---
+++ lesson8/insertValue.php
@@ -0,0 +1,18 @@
+<?php
+try{
+    $pdo = new PDO("mysql:host=localhost;dbname=db", "root", "");
+
+    $username = "Olisa";
+
+    $password = password_hash("mypasword", PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+    $pdo->exec($sql);
+
+    echo "created successfully";
+}catch (Exception $e){
+
+        echo $e->getMessage();
+    }
+?>
