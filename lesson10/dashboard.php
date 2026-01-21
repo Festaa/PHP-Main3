10884df6d5c9dc976be6f5b2488fe0f2fe77cbbf
Festaa <Festa.Rexhepi@digitalschool.tech>
2026-01-21 17:45:03 +0100

    commit

--- lesson10/dashboard.php
+++ lesson10/dashboard.php
@@ -10,10 +10,37 @@
 <?php
     include_once('config.php');
     $sql = "SELECT * FROM users";
-    $getUsers = $conn-prepare($sql);
+    $getUsers = $conn->prepare($sql);
     $getUsers->execute();
     $users=$getUsers->fetchAll();
 ?>
+
+<br><br>
+
+<table>
+    <thead>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Surname</th>
+        <th>Email</th>
+    </thead>
+    <tbody>
+        <?php
+            foreach($users as $user){
+        ?>
+            <tr>
+                <td><?= $user['id'] ?></td>
+                <td><?= $user['name'] ?></td>
+                <td><?= $user['surname'] ?></td>
+                <td><?= $user['email'] ?></td>
+                <td><? "<a href='delete.php?id=$user[id]'> Delete</a>| <a href='edit.php?id=$user[id]'> Update </a>"?></td>
+            </tr>
+            <?php
+            }
+            ?>
+    </tbody>
+</table>
+<a href="add.php">Add user</a>
     
 </body>
 </html>
